782127fa7126228937388498bba68b78dd90a8e5
Sakpak Donald <sakpakd@gmail.com>
2023-03-06 03:44:08 +0100

    create templates with ejs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const Products = require('./products');
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// mongoose.connect("mongodb://localhost:27017/shopDB");
 
-app.get('/', (req, res) => res.render("home"));
+app.get('/', (req, res) => res.render("home", {featuredProducts: Products}));
 app.listen(port, () => console.log(`app listening on port ${port}!`))
---
+++ products.js
@@ -0,0 +1,21 @@
+const products = [{
+    image: "assets/image1.jpg"
+}, {
+    image: "assets/image2.jpg"
+}, {
+    image: "assets/image3.jpg"
+}, {
+    image: "assets/image4.jpg"
+}, {
+    image: "assets/image5.jpg"
+}, {
+    image: "assets/image6.jpg"
+}, {
+    image: "assets/image7.jpg"
+}, {
+    image: "assets/image8.jpg"
+}, {
+    image: "assets/image9.jpg"
+}]
+
+module.exports = products;
