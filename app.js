915608aa23d55fe1c6b98eca424a55602f9971c9
Sakpak Donald <sakpakd@gmail.com>
2023-11-17 22:07:13 +0100

    update environment variables

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
 
-mongoose.connect(`mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.soor9qp.mongodb.net/magicDB`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
